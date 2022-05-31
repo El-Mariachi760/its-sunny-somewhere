c99a5221f8f904f5a5f4066be50e39997f6c264e
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-05-30 23:31:15 -0700

    added save search feature and forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var userFormEl = document.querySelector("#user-form");
 var cityNameInputEl = document.querySelector("#cityname");
 var forecastContainerEl = document.querySelector("#forecast-list");
+var historyBtnEl = document.querySelector("#search-history");
+
+var cities = [];
 
 const apiKey = "a6959ba338d5d0ca6b0761173a65f092";
 
@@ -12,10 +15,17 @@ var formSubmitHandler = function(event) {
   
     if (cityName) {
       getWeather(cityName);
+      get5Day(cityName);
       cityNameInputEl.value = "";
     } else {
       alert("Please enter City Name")
     }
+    saveCity(cityName);
+    searchHistory(cityName);
+};
+
+var saveCity = function(cityName) {
+  localStorage.setItem("cities", JSON.stringify(cityName));
 };
 
 var getWeather = function(cityName) {
@@ -30,39 +40,96 @@ var getWeather = function(cityName) {
       city.innerHTML = nameValue;
       temp.innerHTML = tempValue + " °F";
       desc.innerHTML = descValue;
-      
-      display5Day(data);
       console.log(data.name)
   })
 };
 
-// var get5Day = function(cityName){
-//   debugger
-//   var apiForcastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`
 
-//   fetch(apiForcastURL)
-//   .then(function(response){
-//       response.json().then(function(data){
-//          display5Day(data);
-//       });
-//   });
-// };
 
-var display5Day = function(weather) {
-  debugger
+var get5Day = function(cityName) {
+  console.log(cityName)
   var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`
+  console.log(forecastURL);
   fetch(forecastURL)
   .then (function(response){
     response.json().then(function(data){
-      console.log(data)
+      displayForecast(data)
     })
-  })
-  // console.log(weather)
-  // var forecast = weather.list;
-  //   console.log(forecast);
+  });
+};
 
-}
+function displayForecast(weather) {
+  forecastContainerEl.textContent = ''
+  var forecast = weather.list;
+   for (i=5; i < forecast.length; i=i+8) {
+     var dailyForecast = forecast[i];
+ 
+     var forecastEl=document.createElement("div");
+      //  forecastEl.classList = "card col-lg-4 bg-primary text-light m-3";
+       forecastEl.classList = "card col-lg-3.5 d-flex align-items-stretch bg-primary text-light m-1";
+ 
+       console.log(dailyForecast)
+ 
+       //create date element
+       var forecastDate = document.createElement("h5")
+       forecastDate.textContent= moment.unix(dailyForecast.dt).format("MMM D, YYYY");
+       forecastDate.classList = "card-header text-center"
+       forecastEl.appendChild(forecastDate);
+ 
+       
+       //create an image element
+       var weatherIcon = document.createElement("img")
+       weatherIcon.classList = "card-body text-center";
+       weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);  
+ 
+       //append to forecast card
+       forecastEl.appendChild(weatherIcon);
+       
+       //create temperature span
+       var forecastTempEl=document.createElement("span");
+       forecastTempEl.classList = "card-body text-center";
+       forecastTempEl.textContent = dailyForecast.main.temp + " °F";
+ 
+        //append to forecast card
+        forecastEl.appendChild(forecastTempEl);
+ 
+       var forecastHumEl=document.createElement("span");
+       forecastHumEl.classList = "card-body text-center";
+       forecastHumEl.textContent = "HMD: " + dailyForecast.main.humidity + "  %";
+ 
+       //append to forecast card
+       forecastEl.appendChild(forecastHumEl);
+ 
+        // console.log(forecastEl);
+       //append to five day container
+        forecastContainerEl.appendChild(forecastEl);
+      console.log(dailyForecast);
+   }
+};
 
+var searchHistory = function(searchHistory) {
 
+  historyContainerEl = document.createElement("button");
+  historyContainerEl.textContent = searchHistory;
+  historyContainerEl.classList = "d-flex w-100 btn-light border p-2";
+  historyContainerEl.setAttribute("data-city",searchHistory)
+  historyContainerEl.setAttribute("type", "submit");
 
-  userFormEl.addEventListener("submit", formSubmitHandler);
+  historyBtnEl.prepend(historyContainerEl);
+};
+
+var searchHistoryHandler = function(event) {
+  var cityName = event.target.getAttribute("data-city")
+  if(cityName) {
+    getWeather(cityName);
+    get5Day(cityName);
+  }
+};
+
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#current-day").html(todayDate);
+ 
+ 
+userFormEl.addEventListener("submit", formSubmitHandler);
+historyBtnEl.addEventListener("click", searchHistoryHandler);
+
